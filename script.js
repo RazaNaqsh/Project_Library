31128409af225109d0743f53c525273c1fffa973
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-15 14:17:27 +0530

    fonts imported

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const bookTitle = document.getElementById("bookName").value;
 const authorName = document.getElementById("authorName").value;
 const bookPages = document.getElementById("bookPages").value;
 const bookStatus = document.getElementById("bookStatus");
+const addBtn = document.getElementById("submit");
 
 if (bookStatus.checked === false) {
 	console.log("not read");
 } else console.log("read");
+
+addBtn.addEventListener("click", () => {});
 
 const Library = [];
 
