82e5be6102eb99ae52c7274b20ca42558694768d
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-15 11:53:32 +0530

    Library basic layout done

--- script.js
+++ script.js
@@ -5,5 +5,24 @@ function Book(name, pages, status) {
 	this.pages = pages;
 	this.status = status;
 }
+// Book.prototype.info = function () {
+// 	return `${this.name} `
+// }
+function addBookToLibrary() {
+	// const bookName = prompt("Enter bookName");
+	// const bookPages = parseInt(prompt("enter pages"));
+	// const bookStatus = prompt("Read or UnRead?");
+	// const book = new Book(bookName, bookPages, bookStatus);
+	const book = new Book("The Hobbit", 250, "yes");
+	const book2 = new Book("The bobby", 250, "yes");
+	// console.log(book);
+	Library.push(book);
+	Library.push(book2);
+}
+addBookToLibrary();
+function showCase() {
+	// console.log(Library);
+	Library.forEach((book) => console.table(book));
+}
 
-function addBookToLibrary() {}
+showCase();
