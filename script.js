f91d72aa8862af956bc8de7e1dcdda5eff2108f8
RazaNaqsh <owaisraza1704@gmail.com>
2023-01-16 23:55:08 +0530

    read status works fine for preEntered data

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// const bookStatus = document.getElementById("bookStatus");
 const addBtn = document.getElementById("submit");
 const list = document.getElementById("bookList");
 
@@ -11,19 +10,19 @@ const Library = [
 		title: "Solo Leveling",
 		author: "Chugong",
 		pages: 300,
-		status: "read",
+		status: "notRead",
 	},
 	{
 		title: "Tbate",
 		author: "turtleMe",
 		pages: 700,
-		status: "read",
+		status: "notRead",
 	},
 	{
 		title: "Classroom Of Elite",
 		author: "Syougo Kinugasa",
 		pages: 250,
-		status: "read",
+		status: "Read",
 	},
 ];
 
@@ -33,11 +32,14 @@ function Book(name, author, pages) {
 	this.pages = pages;
 }
 
-function addToList(book, index) {
+function addToList(book, index, status) {
 	const row = document.createElement("tr");
+	let tick;
+	if (status === true) tick = "checked";
 	row.innerHTML = `<td data-index =${index}>${book.title}</td>
 	<td data-index =${index}>${book.author}</td>
 	<td data-index =${index}>${book.pages}</td>
+	<td data-index =${index}><input type="checkbox" class="readStatus" ${tick}></td>
 	<td data-index =${index}><a href="#" data-index =${index} class="cancel">X</a></td>`;
 	list.appendChild(row);
 }
@@ -57,18 +59,32 @@ addBtn.addEventListener("click", (e) => {
 	const bookTitle = document.getElementById("bookName").value;
 	const authorName = document.getElementById("authorName").value;
 	const bookPages = document.getElementById("bookPages").value;
+	const status = document.getElementById("bookStatus");
 	const book = new Book(bookTitle, authorName, bookPages);
+
 	Library.push(book);
 	const indexBook = Library.indexOf(book);
-	addToList(book, indexBook);
+	addToList(book, indexBook, status.checked);
 	RemoveBook();
 	document.getElementById("bookName").value = "";
 	document.getElementById("authorName").value = "";
-	document.getElementById("bookPages").value = undefined;
+	document.getElementById("bookPages").value = 0;
 });
 
 Library.forEach((book) => {
-	addToList(book, Library.indexOf(book));
+	addToList(book, Library.indexOf(book), book.status === "Read");
 });
 
 RemoveBook();
+const bookStatus = Array.from(document.querySelectorAll(".readStatus"));
+bookStatus.forEach((bookstat) => {
+	bookstat.addEventListener("click", (e) => {
+		const readIndex = e.target.parentElement.getAttribute("data-index");
+		if (bookstat.checked === true) {
+			Library[readIndex].status = "Read";
+		} else {
+			Library[readIndex].status = "notRead";
+		}
+		console.log(Library);
+	});
+});
